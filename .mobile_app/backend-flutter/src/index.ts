bd8db23b580a05a4df62b01c13fd1dc015f96d59
Rach <54503978+justrach@users.noreply.github.com>
2024-09-14 23:52:05 -0400

    added resend to send emails out; let the chats and meetups begin

--- .mobile_app/backend-flutter/src/index.ts
+++ .mobile_app/backend-flutter/src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import { Resend } from 'resend'
 
 const app = new Hono()
-const resend = new Resend('re_XKJLkAaz_L8aJZ2skC3kA6f2hvxpHudWq')
+const resend = new Resend('re_GXDzLGsV_ErGWaD87bDVmYufbPvRPeC6T')
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -18,12 +18,16 @@ app.post('/send-email', async (c) => {
 
   try {
     const { data, error } = await resend.emails.send({
-      from: 'HackTheNorth <admin@omni.boopr.xyz>',
+      from: 'admin <admin@excus.us>',
       to: [email],
-      subject: 'Invitation to try excus.us',
-      html: `<p>Dear ${firstName},</p><p>You are now invited to try out <a href="https://excus.us">https://excus.us</a>! The code for hack the north is <strong>htn</strong>.</p><p>Happy hacking!</p>`
-    })
-
+      subject: "We found you a match! Log in to excus.us (Hack the North)",
+      html: `<p>Dear ${firstName},</p>
+      <p>Exciting news! You've been matched with a fellow hacker. To start your conversation, please <a href="https://www.excus.us/">log in to excus.us</a> and begin hacking together!</p>
+      <p>For the best experience, we recommend using the desktop version of our site, as the mobile view may have some minor issues.</p>
+      <p>We hope you enjoy your experience and forge meaningful connections in the tech world!</p>
+      <p>If you need any assistance, feel free to reach out to our support team at <a href="mailto:support@excus.us">support@excus.us</a> or DM Rach @Rach Pradhan on Slack.</p>
+      <p>Happy hacking!</p>`
+          })
     if (error) {
       console.error('Error sending email:', error)
       return c.json({ error: 'Failed to send email' }, 500)
